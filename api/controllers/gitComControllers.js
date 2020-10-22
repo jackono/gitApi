664c4e8c24441f768346064c88a2d46a3bd4d6d9
Jackono Fabella <jackonofabella@gmail.com>
2020-10-22 22:55:07 +0800

    first draft

--- api/controllers/gitComControllers.js
+++ api/controllers/gitComControllers.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const e = require('express');
 var https = require('https');
 const fetch = require("node-fetch");
 var mongoose = require('mongoose'),
@@ -27,7 +28,7 @@ exports.post_a_comment = function(req, res) {
 };
 
 
-exports.user_details = (req, res) => {
+exports.user_details = function(req, res) {
 
   var users = [];
   var options = {
@@ -36,81 +37,90 @@ exports.user_details = (req, res) => {
     method: 'GET',
     headers: {'user-agent': 'node.js'}
   };
-  var request = https.request(options, (response) =>{
+  var request = https.request(options, function(response) {
  
     var body = ''; 
+    var sortedFollowers = ''
     
     
-    response.on("data", (chunk) => {
+    response.on("data", function (chunk) {
         body += chunk.toString('utf8');
     });
     
-    response.on("end", function(err){
+    response.on("end", async function(err){
         
         if (response.statusCode != 200)
            res.send(err);
         var members = JSON.parse(body);
-        // members.forEach(function(list_user) {
-        for(var list_user of members){
-          var options = {
-            host: 'api.github.com',
-            path: '/users/' + list_user.login,
-            method: 'GET',
-            headers: {'user-agent': 'node.js'}
-          };
-          // var request = https.request(options, function(response){
-          //   var body = ''; 
+        // process.nextTick(function next(){
+
+        members.forEach(function(list_user) {
+        // for(var list_user of members){
+          options.path = '/users/' + list_user.login;
+          // return new Promise((resolve, reject) => {
+          var request = https.request(options, function(response){
+            var body = ''; 
          
-          //   response.on("data", function(chunk){
-          //       body += chunk.toString('utf8');
-          //   });
-          //   response.on("end", function(err){
-          //     if (err)
-          //       res.send(err);
-          //     var user = JSON.parse(body);
-          //     var git_user = {
-          //       login: list_user.login,
-          //       avatar_url: list_user.avatar_url,
-          //       num_followers: user.followers,
-          //       num_following: user.following
-          //     };
-          //     // console.log(git_user);
-          //     users.push(git_user);
-          //     console.log(users);
-          //     });
-          //   });
+            response.on("data", function(chunk){
+                body += chunk.toString('utf8');
+            });
+            response.on("end", function(err){
+              if (err)
+                res.send(err);
+              var user = JSON.parse(body);
+              var git_user = {
+                "login": list_user.login,
+                "avatar_url": list_user.avatar_url,
+                "num_followers": user.followers,
+                "num_following": user.following
+              };
+              // console.log(git_user);
+              users.push(git_user);
+              // sortedFollowers = users.sort((low, high) => high.num_followers - low.num_followers);
+              // console.log(users);
+              });
+            });
           
-          const url = 'https://api.github.com/users/' + list_user.login;
+          // const url = 'https://api.github.com/users/' + list_user.login;
 
-          const getData = async url => {
-          try {
-            const response = await fetch(url);
-            const user = await response.json();
-            var git_user = {
-              login: list_user.login,
-              avatar_url: list_user.avatar_url,
-              num_followers: user.followers,
-              num_following: user.following
-            };
-            users.push(git_user);
-            console.log(users);
-          } catch (error) {
-              console.log(error);
-            }
-          };
+          // const getData = async url => {
+          // try {
+          //   const response = await fetch(url);
+          //   const user = await response.json();
+          //   git_user = {
+          //     login: list_user.login,
+          //     avatar_url: list_user.avatar_url,
+          //     num_followers: user.followers,
+          //     num_following: user.following
+          //   };
+          //   users.push(git_user);
+          // } catch (error) {
+          //     console.log(error);
+          //   }
+          //   console.log(users);
+          // };
 
-          getData(url);
+          // getData(url);
+          // users.push(git_user);
           // console.log(users);
-          // request.end();
-          }
-        console.log(users);
-        // res.json(users);
+          // process.nextTick(next);
+          // console.log(users);
+          request.end();
+        });
+        // }
+        await timeout(2000);
+        // sortedFollowers = Object.keys(users).map(function (key){
+        //   return users[key];
+        // }).sort((low, high) => high.num_followers < low.num_followers);
+        sortedFollowers = users.sort((low, high) => high.num_followers - low.num_followers);
+        console.log(sortedFollowers);
+        res.json(sortedFollowers);
         // console.log(JSON.parse(body));
         });
     });
-
+    // console.log(users);
     request.end();
-    res.json(users);
+    // res.json(users);
     
 };
 
@@ -123,3 +133,6 @@ exports.delete_all_comments = function(req, res) {
   });
 };
 
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
